fa80efea06b5816d670c8a05721fc598e96267e0
nimit-pasricha <pasricha.nimit@gmail.com>
2025-09-28 20:49:09 -0500

    Configure SQLAlchemy.

---
+++ backend/app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from .models import db
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
+    return app
