a52b550d227d6affa15d28bc770e6a9a7daf714c
nimit-pasricha <pasricha.nimit@gmail.com>
2025-09-29 19:31:50 -0500

    Change CORS config to handle origin port 5173.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,13 +3,14 @@ from .models import db
 from flask_cors import CORS
 from .routes import bp as api_bp
 
+
 def create_app():
     app = Flask(__name__)
 
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
     db.init_app(app)
 
-    CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
+    CORS(app, resources={r"/api/*": {"origins": "http://localhost:5173"}})
 
     app.register_blueprint(api_bp)
 
