b9d84d83218c855c855959b7f10ecc125b427496
nimit-pasricha <pasricha.nimit@gmail.com>
2025-09-29 19:32:21 -0500

    Implement chatrooms endpoint.

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,3 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
-bp = Blueprint('api', __name__, url_prefix='/api/v1')
+CHATROOMS = [
+    "Bascom Hill Hangout",
+    "Memorial Union Meetups",
+    "Witte Whispers",
+    "Chadbourne Chats",
+    "Red Gym Rendezvous",
+    "Babcock Banter",
+    "Humanities Hubbub",
+]
+
+bp = Blueprint("api", __name__, url_prefix="/api/v1")
+
+
+@bp.route("/chatrooms")
+def get_chatrooms():
+    res = jsonify(CHATROOMS)
+    res.status_code = 200
+    return res
