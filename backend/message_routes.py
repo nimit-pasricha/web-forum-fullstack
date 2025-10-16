766942cece120212c18fefcc1add60ecaa67d116
Nimit Pasricha <pasricha.nimit@gmail.com>
2025-10-16 17:06:48 -0500

    Check if chatroom arg was given in /messages.

--- backend/message_routes.py
+++ backend/message_routes.py
@@ -11,3 +11,13 @@ def get_chatrooms():
     chatroom_names = db.session.execute(chatroom_name_col).scalars().all()
     return jsonify(chatroom_names), 200
 
+@messages_bp.route('/messages', methods=['GET'])
+def get_messages():
+    chatroom_name = request.args.get('chatroom')
+    page = request.args.get('page', 1, type=int)
+
+    if not chatroom_name:
+        return jsonify({"msg": "A 'chatroom' query parameter is required."}), 400
+    if page < 1 or page > 4:
+        return jsonify({"msg": "A page number must be between 1 and 4."}), 400
+    
